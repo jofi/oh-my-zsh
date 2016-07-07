3dcb10ffaf962cdfcf5cacd5c30256c3b306c367
Jozef Fulop <jozef.fulop@innovatrics.com>
2016-07-07 16:59:48 +0200

    introduce git_jofi plugin

---
+++ plugins/git_jofi/git_jofi.plugin.zsh
@@ -0,0 +1,31 @@
+# Use `hub` as our git wrapper:
+#   http://defunkt.github.com/hub/
+hub_path=$(which hub)
+if [[ -f $hub_path ]]
+then
+  alias git=$hub_path
+fi
+
+# The rest of my fun git aliases
+alias gl='git pull --prune'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%ad)%Creset' --abbrev-commit --date=relative"
+alias gshow="git show"
+alias gp='git push origin HEAD'
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status' # upgrade your git if -sb breaks for you. it's fun.
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+
+# Uses git's autocompletion for inner commands. Assumes an install of git's
+# bash `git-completion` script at $completion below (this is where Homebrew
+# tosses it, at least).
+completion=/usr/local/etc/bash_completion.d/git-completion.bash
+
+if test -f $completion
+then
+  source $completion
+fi
+
